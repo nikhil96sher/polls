25c9954c3494c070224c736c76ff0ae485d5c41a
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2015-05-10 23:11:59 +0530

    Part 2 Completed, Part 3 Almost Completed

--- MySite/urls.py
+++ MySite/urls.py
@@ -1,10 +1,4 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'MySite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('', url(r'^polls/',include('polls.urls')),url(r'^admin/', include(admin.site.urls)),)
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from polls.models import Question
+from polls.models import Choice
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra=2
+class Questionadmin(admin.ModelAdmin):
+	list_display=('question_text','pub_date')
+	inlines=[ChoiceInline]
+	list_filter=['pub_date']
+	search_fields=['question_text']
+admin.site.register(Question,Questionadmin)
